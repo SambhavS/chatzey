ebf72275eaeba8fb7109f2b78b19460332e367e6
Emerson Hsieh <emersonhsieh@berkeley.edu>
2018-04-27 22:18:13 -0700

    Changed structure for message socket, default name bob

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/newdb');
+mongoose.connect('mongodb://localhost/newdb2');
 
 const PORTNUM = 3000;
 
@@ -17,8 +17,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-var curid = 0
-
 //On-Connection Socket Functions
 io.on('connection', socket => {
   socket.on('begin session', p => {
@@ -27,19 +25,29 @@ io.on('connection', socket => {
       if (err) return console.error(err);
       console.log("All previous messages");
       console.log(messages);
+
+      console.log("Appending previous messages...");
+
+      for(var i = 0, size = messages.length; i < size ; i++){
+        var msgdict = messages[i];
+        socket.emit("old message", msgdict);
+     }
+
     })
   });
   
-  socket.on('chat message', msg => {
+  socket.on('chat message', msgtable => {
   	//sends to everyone else
-    socket.broadcast.emit('chat message', msg);
+    socket.broadcast.emit('chat message', msgtable);
 
     // saves to mongodb!
-    var newMsg = new Message({ msg: msg, own: true, number: curid});
+    // TODO: own is always to true now!
+    var newMsg = new Message({ name: msgtable[0], msg: msgtable[1] });
+
     newMsg.save((err, newMsg) => {
       if (err) return console.error(err);
-      console.log("Saved Message " + msg);
-      curid = curid + 1;
+      console.log("Message By: " + msgtable[0]);
+      console.log("Message Of: " + msgtable[1]);
     });
 
   });
@@ -59,9 +67,8 @@ db.once('open', function() {
 });
 
 var msgSchema = mongoose.Schema({
-  msg: String,
-  own: Boolean,
-  number: Number
+  name: String,
+  msg: String
 });
 
 var Message = mongoose.model('Message', msgSchema);
--- public/clientscripts.js
+++ public/clientscripts.js
@@ -1,8 +1,16 @@
 function docReady () {
 	var mysocket = io();
-
 	// print out original messages.
+	window.name = "bob";
+	
 	mysocket.emit('begin session', 123);
+	mysocket.on('old message', msg => {
+		if (msg["name"] == window.name) {
+			$('#messages').append($('<li class="ownmessage font-weight-light bg-primary text-light rounded">').text(msg["name"] + ": " + msg["msg"]));
+		} else {
+			$('#messages').append($('<li class="othermessage bg-light text-dark border rounded">').text(msg["name"] + ": " + msg["msg"]));
+		}
+	});
 
 	$('.messageform').submit(() => {
 		//Calls 'chat message' with typed message as msg
@@ -17,14 +25,16 @@ function docReady () {
 			}
 		}else{
 			// Chat message found!
-			mysocket.emit('chat message', window.name + ": " + msg);
-			$('#messages').append($('<li class="ownmessage font-weight-light bg-primary text-light rounded">').text(msg));
+			// send a dictionary that contains both window.name and msg
+			msgtable = [window.name, msg]
+			mysocket.emit('chat message', msgtable);
+			$('#messages').append($('<li class="ownmessage font-weight-light bg-primary text-light rounded">').text(msgtable[0] + ": " + msgtable[1]));
 		}
 		$('#m').val('');
 		return false;
 	});
-	mysocket.on('chat message', msg => {
-		$('#messages').append($('<li class="othermessage bg-light text-dark border rounded">').text(msg));
+	mysocket.on('chat message', msgtable => {
+		$('#messages').append($('<li class="othermessage bg-light text-dark border rounded">').text(msgtable[0] + ": " + msgtable[1]));
 	});
 
 }
